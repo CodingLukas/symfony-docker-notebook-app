89f9d9aba56dce20f180cec6886c84519a4a0bd6
Lukas <lukasimbm@gmail.com>
2020-10-22 12:57:06 +0300

    cleanup

--- app/src/Form/Type/PhoneType.php
+++ app/src/Form/Type/PhoneType.php
@@ -3,14 +3,10 @@
 namespace App\Form\Type;
 
 use App\Entity\Phone;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotNull;
 
